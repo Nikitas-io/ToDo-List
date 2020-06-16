217c4f39cfdc726aee925f7832dc9fa2fc7be034
Nikitas IO <chrononautic@gmail.com>
2020-06-16 18:54:48 +0300

    Added a service for the todos

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  // Input Property
+  @Input() todo: Todo;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      // The Classes that are true, get added to the element.
+      'todo': true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
+  onToggle(todo) {
+    // Toggle in UI
+    todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
   }
 
 }
